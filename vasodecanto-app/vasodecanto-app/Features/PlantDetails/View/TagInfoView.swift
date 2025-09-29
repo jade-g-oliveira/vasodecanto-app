c4006818a5acaf6dadf25cb7b247de5c57e89973
Jade Oliveira <jade.ufrgs@gmail.com>
2025-09-29 16:24:43 -0300

    finish bottomsheet details

---
+++ vasodecanto-app/vasodecanto-app/Features/PlantDetails/View/TagInfoView.swift
@@ -0,0 +1,36 @@
+//
+//  TagInfoView.swift
+//  vasodecanto-app
+//
+//  Created by Jade Oliveira on 29/09/25.
+//
+import SwiftUI
+
+
+struct TagView: View {
+    let text: String
+    let iconName: String
+    let backgroundColor: Color
+    let textColor: Color
+    
+    var body: some View {
+        HStack(spacing: 4) {
+            // 1. Ícone
+            Image(systemName: iconName)
+                .font(.title2)
+                .fontWeight(.bold)
+                
+            
+            // 2. Texto
+            Text(text)
+                .font(.custom("Heebo-Regular", size: 14.0))                
+        }
+        // Padding interno (Espaçamento dentro da tag)
+        .padding(.vertical, 4)
+        .padding(.horizontal, 16)
+        
+        .background(backgroundColor)
+        .foregroundColor(textColor)
+        .cornerRadius(24)
+    }
+}
