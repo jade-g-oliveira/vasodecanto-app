3e009a4c3e72967bddc0b635033a5ec07f0f084c
Jade Oliveira <jade.ufrgs@gmail.com>
2025-10-01 19:07:18 -0300

    added font components

---
+++ vasodecanto-app/vasodecanto-app/DesignSystem/Fonts/FontSize.swift
@@ -0,0 +1,17 @@
+//
+//  FontSize.swift
+//  vasodecanto-app
+//
+//  Created by Jade Oliveira on 29/09/25.
+//
+import SwiftUI
+
+// MARK: - Tamanhos de Fonte Base
+struct FontSize {
+    static let extraSmall: CGFloat = 4.0
+    static let small: CGFloat = 8.0
+    static let caption: CGFloat = 14.0
+    static let body: CGFloat = 16.0
+    static let large: CGFloat = 24.0
+    static let title: CGFloat = 32.0
+}
