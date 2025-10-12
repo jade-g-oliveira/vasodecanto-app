2899f8696638400c61f03ae11a28c2048115a192
Jade Oliveira <jade.ufrgs@gmail.com>
2025-10-12 16:28:10 -0300

    created add to list bottom sheet

---
+++ vasodecanto-app/vasodecanto-app/Features/UserPlantList/View/AddToListBottomSheet.swift
@@ -0,0 +1,14 @@
+//
+//  AddToListBottomSheet.swift
+//  vasodecanto-app
+//
+//  Created by Jade Oliveira on 12/10/25.
+//
+import SwiftUI
+
+struct AddToListBottomSheet: View {
+    
+    var body: some View {
+        ZStack{}
+    }
+}
