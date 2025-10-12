aa0cd0131c9cec2b31c50752c2124971d5ad739e
Bruna Fonsêca <1998bfonseca@gmail.com>
2025-10-12 18:49:54 -0300

    adjust content view

--- vasodecanto-app/vasodecanto-app/ContentView.swift
+++ vasodecanto-app/vasodecanto-app/ContentView.swift
@@ -10,31 +10,12 @@ struct ContentView: View {
     @State private var showingBottomSheet = false
     var body: some View {
         NavigationStack {
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-            }
-            .padding()
-
-            //Exemplo de navegação
-            // O NavigationLink em si
             NavigationLink {
                 // 1. O destino: a View para onde você quer ir
                 LoginView()
             } label: {
-                // 2. O rótulo: o que o usuário vai ver (seu botão!)
-                Text("Ir para Login") // Ele automaticamente parece um botão de navegação
-            }
-            .buttonStyle(.borderedProminent)
-
-            NavigationLink {
-                // 1. O destino: a View para onde você quer ir
-                TabViewContainer(selectedTab: .house)
-            } label: {
-                // 2. O rótulo: o que o usuário vai ver (seu botão!)
-                Text("Ir para home") // Ele automaticamente parece um botão de navegação
+                // 2. O rótulo: o que o usuário vai ver
+                Text("Login")
             }
             .buttonStyle(.borderedProminent)
             Button("Abrir Bottom Sheet") {
@@ -44,6 +25,14 @@ struct ContentView: View {
                 // conteúdo da bottomsheet
                 PlantDetailBottomSheet()
             }
+
+            NavigationLink {
+                TabViewContainer(selectedTab: .house)
+            } label: {
+                Text("Lista de plantas")
+            }
+            .buttonStyle(.borderedProminent)
+
             NavigationLink {
                 ProfileView()
             } label: {
