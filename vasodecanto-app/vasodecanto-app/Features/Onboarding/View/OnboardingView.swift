99a9fc6fa65110abe7dfc84025517aa60c7a1f22
Bruna Fonsêca <1998bfonseca@gmail.com>
2025-11-23 17:19:41 -0300

    code adjust

--- vasodecanto-app/vasodecanto-app/Features/Onboarding/View/OnboardingView.swift
+++ vasodecanto-app/vasodecanto-app/Features/Onboarding/View/OnboardingView.swift
@@ -7,39 +7,46 @@
 import SwiftUI
 struct OnboardingView: View {
     var body: some View {
-        ZStack {
-            Color("PrimaryAppColor").ignoresSafeArea()
-            VStack {
-                // Spacer para empurrar o conteúdo principal para baixo
-                Spacer()
-                BubbleView(text: "Adicione uma planta ao seu ambiente.")
-                NavigationLink {
-                    // MARK: Adicionar navegação para a lista de plantas
-                    TabViewContainer(selectedTab: .house)
-                } label: {
-                    Image(systemName: "plus")
-                        .font(.heeboBoldGiant) // Tamanho grande para o ícone
-                        .foregroundColor(Color("GreenLightColor"))
-                        .padding(Spacing.large) // Padding interno para definir o tamanho do círculo
-                        .background(Color.white)
-                        .clipShape(Circle())
-                        .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
+        NavigationStack {
+            ZStack {
+                Color("PrimaryAppColor").ignoresSafeArea()
+                VStack {
+                    // Spacer para empurrar o conteúdo principal para baixo
+                    Spacer()
+                    BubbleView(text: "Adicione uma planta ao seu ambiente.")
+
+                    NavigationLink {
+                        // MARK: Adicionar navegação para a lista de plantas
+                        TabViewContainer(selectedTab: .house)
+                    } label: {
+                        Image(systemName: "plus")
+                            .font(.heeboBoldGiant) // Tamanho grande para o ícone
+                            .foregroundColor(Color("GreenLightColor"))
+                            .padding(Spacing.large) // Padding interno para definir o tamanho do círculo
+                            .background(Color.white)
+                            .clipShape(Circle())
+                            .shadow(color: .black.opacity(0.2), radius: 10, x: 0, y: 5)
+                    }
+                    .padding(.top, Spacing.small)
+
+                    // Spacer para empurrar o conteúdo para o centro
+                    Spacer()
+                        .frame(height: Spacing.big)
+
+                    NavigationLink {
+                        LoginView()
+                    }
+                    label: {
+                        Text("Já tenho uma conta")
+                            .font(.heeboBoldBody)
+                            .underline()
+                            .foregroundColor(.white)
+                    }
+                    .padding(.bottom, 240)
                 }
-                .padding(.top, Spacing.small)
-                // Spacer para empurrar o conteúdo para o centro
-                Spacer().frame(height: Spacing.big)
-                NavigationLink {
-                    LoginView()
-                }
-                label: {
-                    Text("Já tenho uma conta")
-                        .font(.heeboBoldBody)
-                        .underline()
-                        .foregroundColor(.white)
-                }
-                .padding(.bottom, 240)
-            }.padding(.horizontal, Spacing.big)
-            .navigationBarBackButtonHidden(true)
+                .padding(.horizontal, Spacing.big)
+                .navigationBarBackButtonHidden(true)
+            }
         }
     }
 }
@@ -58,6 +65,7 @@ struct BubbleView: View {
                     RoundedRectangle(cornerRadius: 10)
                         .fill(Color.white)
                         .shadow(color: .black.opacity(0.1), radius: 10, x: 0, y: 3)
+
                     Rectangle()
                         .fill(Color.white)
                         .frame(width: triangleSize, height: triangleSize)
